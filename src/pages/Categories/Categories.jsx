14c9b1369c46edf58648ff026c894f84a022976b
baldwin-sudo <kasmimehdi561@gmail.com>
2025-02-28 13:23:35 -0500

    fixed images urls second time :(

--- src/pages/Categories/Categories.jsx
+++ src/pages/Categories/Categories.jsx
@@ -6,15 +6,15 @@ export default function Categories() {
     <div className="categories-container">
       <Link to="men" className="category-item categ-men">
         <p className="category-title ">Men's clothing</p>
-        <img src="src/assets/men-cat.jpg" alt="" />
+        <img src="/assets/men-cat.jpg" alt="" />
       </Link>
       <Link to="women" className="category-item categ-women">
         <p className="category-title">Women's clothing</p>
-        <img src="src/assets/women-cat.jpg" alt="" />
+        <img src="/assets/women-cat.jpg" alt="" />
       </Link>
       <Link to="jewelery" className="category-item categ-jew">
         <p className="category-title">Jewelery</p>
-        <img src="src/assets/jewelery-cat.jpg" alt="" />
+        <img src="/assets/jewelery-cat.jpg" alt="" />
       </Link>
     </div>
   );
